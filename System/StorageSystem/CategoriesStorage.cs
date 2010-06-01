3b0961ea9b92c07adad6f978165461a4697b08e8
admirer4 <admirer4@cf5ef072-efd0-11de-8eb4-853c26953258>
2010-06-01 13:13:49 +0000

    Added implementation for the CategoriesStorage API

---
+++ System/StorageSystem/CategoriesStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CrawlerNameSpace.Utilities;
+
+namespace CrawlerNameSpace.StorageSystem
+{
+    /**
+     * This interface defines the functionality used in order to save or to get the 
+     * defined categories for specific task
+     */ 
+    interface CategoriesStorage
+    {
+        /**
+         * returns the category list of the specified task
+         */
+        List<Category> getCategories(String taskId);
+
+        /**
+         * this method sets the categories for the task id to the specified list
+         * NOTE: this operation will reset pervious configuration
+         */ 
+        void setCategories(String taskId, List<Category> categoryList);
+    }
+}
--- System/StorageSystem/StorageSystem.cs
+++ System/StorageSystem/StorageSystem.cs
@@ -90,11 +90,150 @@ namespace CrawlerNameSpace.StorageSystem
  
         }
 
+        // Start of Catergories Section
+
+        /**
+         * returns the category list of the specified task
+         */
+        public List<Category> getCategories(String taskId)
+        {
+            List<Category> categoryList = new List<Category>();
+            SqlConnection conn = null;
+            SqlDataReader rdr  = null;
+
+            try
+            {
+                conn = new SqlConnection(SettingsReader.getConnectionString());
+                
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT CategoryID,CategoryName,Keywords,ParentCategory,ConfidenceLevel" +
+                    " FROM Category WHERE TaskID=\'" + taskId + "\'", conn);
+
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    int confidenceLevel = Convert.ToInt32(rdr["ConfidenceLevel"]);
+                    List<String> keywords = new List<string>(((String)rdr["Keywords"]).Split(';'));
+                    Category category = new Category(rdr["CategoryID"].ToString(), rdr["ParentCategory"].ToString(), 
+                        rdr["CategoryName"].ToString().Trim(), keywords, confidenceLevel);
+
+                    categoryList.Add(category);
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("Exception Caught: " + e.Message);
+            }
+            finally
+            {
+                if (rdr != null) rdr.Close();
+                if (conn != null) conn.Close();
+            }
+            return categoryList;
+        }
+
+        /**
+         * resets the predifined categories in the specified task
+         * it will return the number of rows which has been removed due to this reset
+         */
+        public int resetCategories(String taskId)
+        {
+            SqlConnection conn = null;
+            int rowsRemoved = 0;
+
+            try
+            {
+                conn = new SqlConnection(SettingsReader.getConnectionString());
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Category WHERE TaskID=\'" + taskId + "\'", conn);
+
+                rowsRemoved = cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("Exception Caught: " + e.Message);
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+            return rowsRemoved;
+        }
+
+        /**
+         * sets the categories for the specified task
+         */
+        public void setCategories(String taskId, List<Category> categoryList)
+        {
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = new SqlConnection(SettingsReader.getConnectionString());
+
+                conn.Open();
+                foreach(Category category in categoryList)
+                {
+                    String keywords = "";
+                    bool flag = true;
+                    foreach(string token in category.getKeywordList())
+                    {
+                        if (flag == true)
+                        {
+                            keywords = token;
+                            flag = false;
+                        }
+                        else keywords = keywords + ";" + token;
+                    }
+
+                    SqlCommand cmd = null;
+                    if(category.getParentName() != null)
+                    {
+                        cmd = new SqlCommand("INSERT INTO Category (TaskID,CategoryName,Keywords,ParentCategory,ConfidenceLevel)"
+                            + " Values (\'" + taskId + "\',\'" + category.getCatrgoryName() + "\',\'" + keywords + "\',\'"  
+                            + category.getParentName() + "\'," + category.getConfidenceLevel().ToString() + ")", conn);
+                    }
+                    else
+                    {
+                        cmd = new SqlCommand("INSERT INTO Category (TaskID,CategoryName,Keywords,ConfidenceLevel)"
+                            + " Values (\'" + taskId + "\',\'" + category.getCatrgoryName() + "\',\'" + keywords + "\',"  
+                            + category.getConfidenceLevel().ToString() + ")", conn);
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("Exception Caught: " + e.Message);
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+        }
+
         public static void Main(String[] args)
         {
             StorageSystem ss = new StorageSystem();
-            List<TaskStatus> l = ss.getWorkDetails("13dba25a-3401-4766-b00a-fcea8a69cefc",QueryOption.IdleTasks);
-            System.Console.WriteLine(QueryOption.IdleTasks);
+            //List<TaskStatus> l = ss.getWorkDetails("13dba25a-3401-4766-b00a-fcea8a69cefc",QueryOption.IdleTasks);
+            //System.Console.WriteLine(QueryOption.IdleTasks);
+            List<String> keywords = new List<string>();
+            keywords.Add("woo"); keywords.Add("moo"); keywords.Add("foo");
+            Category t1 = new Category("", null, "Temp1", keywords, 10);
+            Category t2 = new Category("", null, "Temp1->2", keywords, 10);
+            List<Category> ll = new List<Category>();
+            ll.Add(t1); ll.Add(t2);
+
+            ss.setCategories("13dba25a-3401-4766-b00a-fcea8a69cefc",ll);
+            /*
+            foreach(Category cat in ll)
+            {
+                //String df;
+                //df.Trim();
+                System.Console.WriteLine(cat);
+            }
+            */
         }
     }
 }
