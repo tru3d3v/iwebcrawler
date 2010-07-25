6fe0131649b01988b64e3303259ec7861851153b
admirer4 <admirer4@cf5ef072-efd0-11de-8eb4-853c26953258>
2010-07-25 13:23:24 +0000

    Fix to the last ci.

---
+++ trunk/System/StorageSystem/SettingsStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrawlerNameSpace.StorageSystem
+{
+    /**
+     * This interface contains 
+     */ 
+    interface SettingsStorage
+    {
+    }
+}
