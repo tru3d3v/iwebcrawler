b43a1f4b0bb615b4a284310d7cfdc20faab87255
admirer4 <admirer4@cf5ef072-efd0-11de-8eb4-853c26953258>
2010-09-14 18:36:06 +0000

    CrawlerOptions.aspx.cs added (cont to the prev ci)

---
+++ System/iWeb/CrawlerOptions.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CrawlerNameSpace.StorageSystem;
+using CrawlerNameSpace.Utilities;
+
+public partial class CrawlerOptions : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Text = "Crawler Options";
+        LabelUIVersion.Text = Version.getUIVersion();
+        LabelEngineVersion.Text = Version.getCoreVersion();
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
