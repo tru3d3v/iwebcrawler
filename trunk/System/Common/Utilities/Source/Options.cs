3d4e16578e9fc0e6fbbc273f00ea68e1977dbbdc
admirer4 <admirer4@cf5ef072-efd0-11de-8eb4-853c26953258>
2010-09-22 15:18:19 +0000

    added Options class (fix)

---
+++ trunk/System/Common/Utilities/Source/Options.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrawlerNameSpace.Utilities
+{
+    // This enumuration is used to specify the proporties option for the task
+    public enum TaskProperty { 
+        // General crawling options
+        SEED, 
+        RESTRICT, 
+        ALLOW, 
+        // threads option
+        THREADS,
+        // frontier
+        FRONTIER,
+        // categorizer options
+        CAT_ALPHA, 
+        CAT_BETA, 
+        CAT_GAMMA, 
+        CAT_MIN, 
+        CAT_PENLTY,
+        // nearby options
+        NER_ALPHA,
+        NER_BETA,
+        NER_GAMMA,
+        NER_MIN,
+        NER_PENLTY,
+        // anchor options
+        ANC_ALPHA,
+        ANC_BETA,
+        ANC_GAMMA,
+        ANC_MIN,
+        ANC_PENLTY,
+        // ranker options
+        RAN_ALPHA,
+        RAN_BETA,
+        RAN_GAMMA,
+        RAN_DELTA,
+        RAN_NEARBY
+    };
+    // This enumuration is used to specify the frontier designs
+    public enum FrontierDesign {
+        FIFO_BFS,
+        RANK_SSEv0
+    };
+    // has the selection of the supported frointer designs in the crawler
+    public class supportedFrontierDesigns
+    {
+        static public FrontierDesign[] selection =
+        {
+            FrontierDesign.FIFO_BFS,
+            FrontierDesign.RANK_SSEv0
+        };
+    };
+}
