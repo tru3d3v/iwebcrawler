c9e1184b90cccac1bfdde818375ac9b735479999
admirer4 <admirer4@cf5ef072-efd0-11de-8eb4-853c26953258>
2010-09-23 12:14:57 +0000

    added Ranking Trie and it's utils implementation (1st rev)

---
+++ trunk/System/Common/Utilities/Source/PointingNode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrawlerNameSpace.Utilities
+{
+    /**
+     * This class represents a pointing node which points to the rankNode itself
+     * and the rankTableEntry which contains it
+     */
+    class PointingNode
+    {
+        private RankTableEntry _rankTableEntry;
+        private LinkedListNode<RankNode> _rankNode;
+
+        public PointingNode()
+        {   
+        }
+
+        /**
+         * sets the rankTableEntry
+         */
+        public void setRankTableEntry(RankTableEntry rankTableEntry)
+        {
+            _rankTableEntry = rankTableEntry;
+        }
+
+        /**
+         * sets the RankNode
+         */
+        public void setRankNode(LinkedListNode<RankNode> rankNode)
+        {
+            _rankNode = rankNode;
+        }
+
+        /**
+         * returns the rankTableEntry
+         */
+        public RankTableEntry getRankTableEntry()
+        {
+            return _rankTableEntry;
+        }
+
+        /**
+         * returns the rankNode
+         */
+        public LinkedListNode<RankNode> getRankNode()
+        {
+            return _rankNode;
+        }
+    }
+}
