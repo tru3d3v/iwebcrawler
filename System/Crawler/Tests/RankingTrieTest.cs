42b72ae99eec6adb9f532b192b34b2ddb2d622b4
admirer4 <admirer4@cf5ef072-efd0-11de-8eb4-853c26953258>
2010-10-07 15:24:10 +0000

    RankFrontierTest was added and RankingTrieTest was modified.

--- System/Crawler/Tests/RankingTrieTest.cs
+++ System/Crawler/Tests/RankingTrieTest.cs
@@ -36,11 +36,18 @@ namespace CrawlerNameSpace.Tests
                 Console.WriteLine("The new maximum url is :" + pop.getUrl());
                 pop = testTrie.pop();
             }
+
+            char c;
+            for (int i = 0; i < 93; i++)
+            {
+                c = Convert.ToChar(i+33);
+                Console.WriteLine(c);
+            }
         }
 
         public void Test2()
         {
-            char[] arbitrary=new char[20];
+            //char[] arbitrary=new char[20];
 
             DateTime startTime = DateTime.Now;
             for (int i = 0; i < 500000; i++)
@@ -63,8 +70,84 @@ namespace CrawlerNameSpace.Tests
 
         private char[] generateString()
         {
-            char[] arb = new char[20];
+            char[] arb = new char[50];
+            Random rand = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                
+                int randNum = rand.Next(33, 126);
+                arb[i] = Convert.ToChar(randNum);
+            }
             return arb;
+        }
+
+        public void test3()
+        {
+            DateTime startTime = DateTime.Now;
+            //DateTime startAddingTime = DateTime.Now;
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 5000000; i++)
+            {
+                char[] arb = generateString();
+                String url = new String(arb);
+
+                //if ((i % 1000) == 0) Console.WriteLine(url);
+                testTrie.add(new Url(url, 0,rand.Next(0, 100), url, i));
+            }
+            DateTime endAddingTime = DateTime.Now;
+
+            TimeSpan addingTime = endAddingTime - startTime;
+            
+            Url pop = testTrie.pop();
+            int k = 0;
+            while (pop != null)
+            {
+                //Console.WriteLine("The new max is : " + testTrie.getMaxRank());
+                //Console.WriteLine("The new min is : " + testTrie.getMinRank());
+                if ((k % 10) == 0)
+                    Console.WriteLine("The new maximum url is :" + pop.getUrl());
+                pop = testTrie.pop();
+                k++;
+            }
+             
+            DateTime endAllTime = DateTime.Now;
+            TimeSpan popingTime = endAllTime - endAddingTime;
+            TimeSpan totalTime = endAllTime - startTime;
+
+        }
+
+        public void test4()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                testTrie.add(new Url("www.game.com", 0, 100, "www.game.com", 3));
+            }
+
+            Url pop = testTrie.pop();
+
+            if (pop != null) Console.WriteLine(pop.getUrl() + " " + pop.getRank());
+
+        }
+
+        public void test5()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 600000; i++)
+            {
+                testTrie.add(new Url(i.ToString(), i, rand.Next(0, 100), i.ToString(), 600000 - i));
+            }
+
+            Url pop = testTrie.pop();
+            int k = 0;
+            while (pop != null)
+            {
+                if ((k % 100) == 0)
+                    Console.WriteLine("The new maximum url is :" + pop.getUrl());
+                pop = testTrie.pop();
+                k++;
+            }
         }
     }
 }
