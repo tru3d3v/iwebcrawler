6bb5691e8270623ad9a89d676baa78cb50131f11
admirer4 <admirer4@cf5ef072-efd0-11de-8eb4-853c26953258>
2010-10-10 18:03:50 +0000

    added method tests for the frontier.

--- trunk/System/Crawler/Tests/RankFrontierTest.cs
+++ trunk/System/Crawler/Tests/RankFrontierTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using CrawlerNameSpace.Utilities;
 using System.Threading;
+using System.IO;
 
 namespace CrawlerNameSpace.Tests
 {
@@ -81,6 +82,7 @@ namespace CrawlerNameSpace.Tests
             }
         }
 
+        //test for measuring performance
         public void test2()
         {
             initQueuesTest2();
@@ -88,18 +90,21 @@ namespace CrawlerNameSpace.Tests
             RankFrointer rankFrontier = new RankFrointer(feedback, serverQueues);
             Thread frontierThread = new Thread(new ThreadStart(rankFrontier.sceduleTasks));
             frontierThread.Start();
-            Thread workerThread = new Thread(new ThreadStart(workerSimulator));
+            Thread workerThread = new Thread(new ThreadStart(workerSimulator2));
             workerThread.Start();
             
-            int iteration = 5001;
+            int iteration = 5000;
             while (true)
             {
-                Thread.Sleep(1000);
-                //fillQueue(iteration, iteration + 20);
-                iteration ++;
-                if (iteration < 10000) break;
+                Thread.Sleep(10);
+                fillQueue(iteration, iteration + 5000);
+                iteration += 5000;
+                if (iteration > 1000000)
+                    break;
             }
-            
+            //frontierThread.Join();
+            workerThread.Join();
+
             DateTime endTime = DateTime.Now;
             TimeSpan totalTime = endTime - startTime;
         }
@@ -110,25 +115,198 @@ namespace CrawlerNameSpace.Tests
             fillQueue(0, 5000);
         }
 
+        //test for the bfs
         public void test3()
         {
-            initQueues();
+            initQueuesTest2();
             DateTime startTime = DateTime.Now;
-            BFSFrontier rankFrontier = new BFSFrontier(feedback, serverQueues);
-            Thread frontierThread = new Thread(new ThreadStart(rankFrontier.sceduleTasks));
+            BFSFrontier bfsFrontier = new BFSFrontier(feedback, serverQueues);
+            Thread frontierThread = new Thread(new ThreadStart(bfsFrontier.sceduleTasks));
             frontierThread.Start();
-            Thread workerThread = new Thread(new ThreadStart(workerSimulator));
+            Thread workerThread = new Thread(new ThreadStart(workerSimulator2));
             workerThread.Start();
 
             int iteration = 5000;
-            while (iteration < 1000000)
+            while (true)
             {
-                Thread.Sleep(1000);
-                iteration += 20;
-                fillQueue(iteration, iteration + 19);
+                Thread.Sleep(10);
+                fillQueue(iteration, iteration + 5000);
+                iteration += 5000;
+                if (iteration > 1000000)
+                    break;
             }
+            //frontierThread.Join();
+            workerThread.Join();
+
             DateTime endTime = DateTime.Now;
             TimeSpan totalTime = endTime - startTime;
-        }  
+        }
+
+        private void workerSimulator3()
+        {
+            int errCount = 0, emptyQueueCount = 0;
+            bool queueFirstNotEmpty = false;
+            while (true)
+            {
+                Thread.Sleep(10);
+                    
+                if (SyncAccessor.queueSize<Url>(serverQueues[0]) > 0)
+                {
+                    queueFirstNotEmpty = true;
+                    emptyQueueCount = 0;
+                    Url url = SyncAccessor.getFromQueue<Url>(serverQueues[0], 100);
+                    if (url != null)
+                    {
+                        RuntimeStatistics.addToCrawledUrls(1);
+                    }
+                    else
+                    {
+                        RuntimeStatistics.addToErrors(1);
+                        errCount++;
+                        Console.WriteLine("ERROR - " + errCount.ToString());
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    if (emptyQueueCount > 2)
+                        break;
+                    if (queueFirstNotEmpty)
+                        emptyQueueCount++;
+
+
+                }
+            }
+        }
+
+        //test that displays statistics
+        public void test4()
+        {
+            RuntimeStatistics.addToExtractedUrls(1);
+            initQueuesTest2();
+            RuntimeStatistics.addToFeedUrls(5000);
+            DateTime startTime = DateTime.Now;
+            RankFrointer rankFrontier = new RankFrointer(feedback, serverQueues);
+            Thread frontierThread = new Thread(new ThreadStart(rankFrontier.sceduleTasks));
+            frontierThread.Start();
+            Thread workerThread = new Thread(new ThreadStart(workerSimulator3));
+            workerThread.Start();
+
+            int iteration = 5000;
+            while (true)
+            {
+                StatusDisplay.DisplayOnScreen(feedback, serverQueues);
+                Thread.Sleep(1000);
+                if (iteration > 40000)
+                {
+                    break;
+                }
+                fillQueue(iteration,iteration+5000);
+                iteration += 5000;
+                RuntimeStatistics.addToFeedUrls(5000);
+            }
+            while (true)
+            {
+                StatusDisplay.DisplayOnScreen(feedback, serverQueues);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void workerSimulator2()
+        {
+            int errCount = 0, emptyQueueCount = 0, urlsNum = 1;
+            bool queueFirstNotEmpty = false;
+            while (true)
+            {
+                //Thread.Sleep(10);
+                if (SyncAccessor.queueSize<Url>(serverQueues[0]) > 0)
+                {
+                    queueFirstNotEmpty = true;
+                    emptyQueueCount = 0;
+                    Url url = SyncAccessor.getFromQueue<Url>(serverQueues[0], 100);
+                    if (url != null)
+                    {
+                        Console.WriteLine("******************************************************");
+                        Console.WriteLine("URL DEQUEUED: ");
+                        Console.WriteLine(url.getUrl());
+                        Console.WriteLine("RANK OF URL");
+                        Console.WriteLine(url.getRank());
+                        Console.WriteLine("NumberOfUrls");
+                        Console.WriteLine(urlsNum++);
+                    }
+                    else
+                    {
+                        errCount++;
+                        Console.WriteLine("ERROR - " + errCount.ToString());
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    if (emptyQueueCount > 2)
+                        break;
+                    if (queueFirstNotEmpty)
+                        emptyQueueCount++;
+
+
+                }
+            }
+        }
+
+        public void test5()
+        {
+
+            initQueuesTest2();
+            RuntimeStatistics.addToExtractedUrls(1);
+            RuntimeStatistics.addToFeedUrls(5000);
+            RankFrointer rankFrontier = new RankFrointer(feedback, serverQueues);
+            Thread frontierThread = new Thread(new ThreadStart(rankFrontier.sceduleTasks));
+            frontierThread.Start();
+            Thread workerThread = new Thread(new ThreadStart(workerSimulator4));
+            workerThread.Start();
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                StatusDisplay.DisplayOnScreen(feedback, serverQueues);
+            }
+
+        }
+
+        private void workerSimulator4()
+        {
+            int errCount = 0, urlsNum = 1,iterations=5000;
+            while (true)
+            {
+                fillQueue(iterations, iterations + 10);
+                RuntimeStatistics.addToFeedUrls(10);
+                iterations += 100;
+                Thread.Sleep(10);
+                if (SyncAccessor.queueSize<Url>(serverQueues[0]) > 0)
+                {
+                    Url url = SyncAccessor.getFromQueue<Url>(serverQueues[0], 100);
+                    if (url != null)
+                    {
+                        RuntimeStatistics.addToCrawledUrls(1);
+                        StreamWriter sw = new
+                        StreamWriter("FrontierDebugger.txt", true);
+                        sw.WriteLine("******************************************************");
+                        sw.WriteLine("URL DEQUEUED: ");
+                        sw.WriteLine(url.getUrl());
+                        sw.WriteLine("RANK OF URL");
+                        sw.WriteLine(url.getRank());
+                        sw.WriteLine("NumberOfUrls");
+                        sw.WriteLine(urlsNum++);
+                        sw.Close();
+                    }
+                    else
+                    {
+                        errCount++;
+                        Console.WriteLine("ERROR - " + errCount.ToString());
+                    }
+                }
+
+            }
+        }
     }
 }
