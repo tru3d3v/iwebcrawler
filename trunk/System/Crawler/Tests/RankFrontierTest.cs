90717db139062033653b17f9d9285985cb776a45
admirer4 <admirer4@cf5ef072-efd0-11de-8eb4-853c26953258>
2010-10-12 14:29:21 +0000

    added several tests to RankFrontierTest.cs

--- trunk/System/Crawler/Tests/RankFrontierTest.cs
+++ trunk/System/Crawler/Tests/RankFrontierTest.cs
@@ -99,7 +99,7 @@ namespace CrawlerNameSpace.Tests
                 Thread.Sleep(10);
                 fillQueue(iteration, iteration + 5000);
                 iteration += 5000;
-                if (iteration > 1000000)
+                if (iteration > 50000)
                     break;
             }
             //frontierThread.Join();
@@ -132,7 +132,7 @@ namespace CrawlerNameSpace.Tests
                 Thread.Sleep(10);
                 fillQueue(iteration, iteration + 5000);
                 iteration += 5000;
-                if (iteration > 1000000)
+                if (iteration > 50000)
                     break;
             }
             //frontierThread.Join();
@@ -275,21 +275,21 @@ namespace CrawlerNameSpace.Tests
 
         private void workerSimulator4()
         {
-            int errCount = 0, urlsNum = 1,iterations=5000;
+            int errCount = 0, urlsNum = 1,emptyQueueCount = 0;
+            bool queueFirstNotEmpty = false;
             while (true)
             {
-                fillQueue(iterations, iterations + 10);
-                RuntimeStatistics.addToFeedUrls(10);
-                iterations += 100;
                 Thread.Sleep(10);
                 if (SyncAccessor.queueSize<Url>(serverQueues[0]) > 0)
                 {
+                    queueFirstNotEmpty = true;
+                    emptyQueueCount = 0;
                     Url url = SyncAccessor.getFromQueue<Url>(serverQueues[0], 100);
                     if (url != null)
                     {
                         RuntimeStatistics.addToCrawledUrls(1);
                         StreamWriter sw = new
-                        StreamWriter("FrontierDebugger.txt", true);
+                        StreamWriter("FrontierDebugger1.txt", true);
                         sw.WriteLine("******************************************************");
                         sw.WriteLine("URL DEQUEUED: ");
                         sw.WriteLine(url.getUrl());
@@ -305,8 +305,113 @@ namespace CrawlerNameSpace.Tests
                         Console.WriteLine("ERROR - " + errCount.ToString());
                     }
                 }
+                else
+                {
+                    Thread.Sleep(1000);
+                    if (emptyQueueCount > 2)
+                        break;
+                    if (queueFirstNotEmpty)
+                        emptyQueueCount++;
+                }
 
             }
+        }
+
+        private void workerSimulator5()
+        {
+            int errCount = 0, urlsNum = 1, emptyQueueCount = 0;
+            bool queueFirstNotEmpty = false;
+            while (true)
+            {
+                Thread.Sleep(10);
+                if (SyncAccessor.queueSize<Url>(serverQueues[1]) > 0)
+                {
+                    queueFirstNotEmpty = true;
+                    emptyQueueCount = 0;
+                    Url url = SyncAccessor.getFromQueue<Url>(serverQueues[1], 100);
+                    if (url != null)
+                    {
+                        RuntimeStatistics.addToCrawledUrls(1);
+                        StreamWriter sw = new
+                        StreamWriter("FrontierDebugger2.txt", true);
+                        sw.WriteLine("******************************************************");
+                        sw.WriteLine("URL DEQUEUED: ");
+                        sw.WriteLine(url.getUrl());
+                        sw.WriteLine("RANK OF URL");
+                        sw.WriteLine(url.getRank());
+                        sw.WriteLine("NumberOfUrls");
+                        sw.WriteLine(urlsNum++);
+                        sw.Close();
+                    }
+                    else
+                    {
+                        errCount++;
+                        Console.WriteLine("ERROR - " + errCount.ToString());
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    if (emptyQueueCount > 2)
+                        break;
+                    if (queueFirstNotEmpty)
+                        emptyQueueCount++;
+                }
+
+            }
+        }
+
+        //tests a multi-threaded workers
+        public void test6()
+        {
+            DateTime startTime = DateTime.Now;
+            initQueuesTest3();
+            RuntimeStatistics.addToExtractedUrls(1);
+            RuntimeStatistics.addToFeedUrls(5000);
+            RankFrointer rankFrontier = new RankFrointer(feedback, serverQueues);
+            Thread frontierThread = new Thread(new ThreadStart(rankFrontier.sceduleTasks));
+            frontierThread.Start();
+            Thread workerThread = new Thread(new ThreadStart(workerSimulator4));
+            workerThread.Start();
+
+            Thread workerThread2 = new Thread(new ThreadStart(workerSimulator5));
+            workerThread2.Start();
+
+            int iteration = 5000;
+            while (true)
+            {
+                Thread.Sleep(10);
+                fillQueue(iteration, iteration + 5000);
+                iteration += 5000;
+                RuntimeStatistics.addToFeedUrls(5000);
+                StatusDisplay.DisplayOnScreen(feedback, serverQueues);
+                if (iteration > 1000000)
+                    break;
+            }
+            while (true)
+            {
+                Thread.Sleep(1000);
+                StatusDisplay.DisplayOnScreen(feedback, serverQueues);
+                //workerThread.Interrupt();
+                ThreadState state = workerThread.ThreadState;
+                Console.WriteLine("Workerthread is : " + state.ToString());
+                ThreadState state2 = workerThread2.ThreadState;
+                if ((state == ThreadState.Stopped)&&(state2==ThreadState.Stopped))
+                {
+                    //continue;
+                    //workerThread2.Join();
+                    DateTime endTime = DateTime.Now;
+                    TimeSpan totalTime = endTime - startTime;
+                }
+                Console.WriteLine("Workerthread2 is : " + state2.ToString());
+            }
+        }
+
+        private void initQueuesTest3()
+        {
+            serverQueues.Add(new Queue<Url>());
+            serverQueues.Add(new Queue<Url>());
+            fillQueue(0, 5000);
         }
     }
 }
